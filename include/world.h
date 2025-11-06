dfa729501bbb11aa93a999998da550ae546d18e9
Lucca <luccadangelo10@gmail.com>
2025-11-06 09:17:08 -0300

    Ajustando o world.h

--- include/world.h
+++ include/world.h
@@ -5,16 +5,18 @@
 
 #define MAX_LANES 12
 #define MAX_CARS_PER_LANE 8
+#define NUM_CAR_TEXTURES 3
 
 typedef struct {
     Rectangle box;
-    float speed;
+    float baseSpeed;     // era "speed" no seu header
     int dir;
     bool active;
+    int textureIndex;    // usado no world.c
 } Car;
 
 typedef struct {
-    int y;
+    float yTop;                  // era "int y" no seu header
     bool isRoad;
     int carCount;
     Car cars[MAX_CARS_PER_LANE];
@@ -23,12 +25,31 @@ typedef struct {
 typedef struct {
     int laneCount;
     float tile;
+    float worldWidth;            // usado no world.c
+
+    // Texturas usadas no world.c
+    Texture2D texPavement;
+    Texture2D texRoad;
+    Texture2D texCar[NUM_CAR_TEXTURES];
+
     Lane lanes[MAX_LANES];
 } World;
 
-void World_Init(World *w, int screenW, int screenH, float tile);
-void World_Update(World *w, float dt, int screenW);
-void World_Draw(const World *w, Vector2 cameraOffset);
+// Assinaturas como no world.c
+void World_Init(World *w, float worldWidth, float tile);
+void World_Update(World *w, float dt, float difficulty);
+void World_Draw(const World *w, Rectangle visibleWorldRect);
 bool World_CheckCollision(const World *w, Rectangle player);
+bool World_CheckCollisionInset(const World *w, Rectangle player, float inset);
+
+// Se quiser usar fora do .c, não deixe static:
+float randf(float a, float b);
+void DrawTextureTiledCompat(Texture2D tex,
+                            Rectangle src,
+                            Rectangle dest,
+                            Vector2 origin,
+                            float rotation,
+                            float scale,
+                            Color tint);
 
 #endif
